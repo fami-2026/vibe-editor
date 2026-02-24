db68890a95a30c72e4d0c71f946fdf3d910f4994
Вадим Герасименко <vadim2016gerasimenko@gmail.com>
2026-02-24 17:55:52 +0700

    feature: #19 / #20 / #21: docs storage and management

---
+++ server/src/main/java/org/editor/control/service/CanvasServiceImpl.java
@@ -0,0 +1,68 @@
+package org.editor.control.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.editor.control.dao.CanvasRepository;
+import org.editor.control.handler.exception.CanvasNotExistException;
+import org.editor.control.handler.exception.EditorValidationException;
+import org.editor.control.mapping.CanvasMapper;
+import org.editor.entity.dto.CanvasMeta;
+import org.editor.entity.mongo.Canvas;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static java.util.Objects.*;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CanvasServiceImpl implements CanvasService {
+
+    private final CanvasRepository canvasRepository;
+
+    private final CanvasMapper canvasMapper;
+
+    @Override
+    public List<CanvasMeta> getAllMeta() {
+        return canvasRepository.findAll().stream()
+                .map(canvasMapper::mapToMeta)
+                .toList();
+    }
+
+    @Override
+    public Canvas get(String id) {
+        return canvasRepository.findById(id).orElseThrow(CanvasNotExistException::new);
+    }
+
+    @Override
+    public CanvasMeta create(String title, String state) {
+        if (isNull(title) || isNull(state) || title.isBlank() || state.isBlank()) {
+            throw new EditorValidationException("Not all required fields are represented");
+        }
+
+        Canvas canvas = canvasRepository.save(canvasMapper.mapToCanvas(title, state));
+        return canvasMapper.mapToMeta(canvas);
+    }
+
+    @Override
+    public CanvasMeta update(String id, String title, String state) {
+        Canvas canvas = get(id);
+
+        if (nonNull(title)) {
+            canvas.setTitle(title);
+        }
+
+        if (nonNull(canvas.getState())) {
+            canvas.setState(canvas.getState());
+        }
+
+        canvas = canvasRepository.save(canvas);
+        return canvasMapper.mapToMeta(canvas);
+    }
+
+    @Override
+    public void delete(String id) {
+        canvasRepository.delete(get(id));
+    }
+}
---
+++ server/src/main/java/org/editor/entity/mongo/Canvas.java
@@ -0,0 +1,37 @@
+package org.editor.entity.mongo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.FieldType;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "canvas")
+public class Canvas {
+
+    @MongoId(FieldType.STRING)
+    private String id;
+
+    private String title;
+
+    private String state;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    public Canvas(String title, String state) {
+        this.title = title;
+        this.state = state;
+    }
+}
